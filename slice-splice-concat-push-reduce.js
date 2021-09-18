9ef456ae19b79369c4805983df32da72e5d0fbb3
Coders Key <designer.afride@gmail.com>
2021-09-17 21:13:20 -0700

    first commit

--- slice-splice-concat-push-reduce.js
+++ slice-splice-concat-push-reduce.js
@@ -2,10 +2,9 @@
 // var result = numbers.slice(1, 3)
 // console.log(result)
 
-// var numbers = [12, 14, 16, 17, 19, 20]
-// var result = numbers.splice(1, 3, 7, 8, 9, 8)
-// console.log(numbers);
-// console.log(result);
+var numbers = [12, 14, 16, 17, 19, 20, 7, 8, 9]
+var splice = numbers.splice(1, 4, 188, 666, 888, 3333) // start, koita katbo, kata jaigai baki gulu dukbe
+console.log('splice', splice);
 
 // var numbers1 = [12, 14, 16, 17, 19, 20];
 // var numbers2 = [5, 7, 9, 7];
